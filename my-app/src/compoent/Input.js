3116f0dd5fdf465fb34f35ce8f981f1bf515129a
Avijit pal <avijitpal20309@gmail.com>
2021-07-15 13:44:35 +0530

    array needs to be created

--- my-app/src/compoent/Input.js
+++ my-app/src/compoent/Input.js
@@ -6,99 +6,80 @@ class Inputfield extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-     maindata: {
       //  months:0,
-        Amount: 0,
-        numberofmonths:0,
-        whichmonth:''
-      }
-    };
-
-
-    //this.handleInputChange1= this.handleInputChange1.bind(this);
+        Amount: [],
+        numberofmonths:[],
+        whichmonth:'',
+    };    
     this.handleInputChange1= this.handleInputChange1.bind(this);
     this.handleInputChange2= this.handleInputChange2.bind(this);
     this.handleInputChange3= this.handleInputChange3.bind(this);
-  }
-  //const data =[Amount:]
-  
-  //handleInputChange1(event) {
-   //  var data1 = this.state.maindata;
-     // data1.months = event.target.value;
-      //value = {this.state.maindata.months};
-    //this.setState({
-    //  miandata : data1
-    //});
- // }
- //const Data = JSON.stringify(Object);
-
-  handleInputChange1(event) {
-       var data1 = this.state.maindata;
-       data1.Amount = event.target.value;
+    
      
-    // value = {this.state.maindata.Amount};
+  }
+   
+  handleInputChange1(event) {
+       var data1 = this.state;
+       data1.Amount = event.target.value;
        this.setState({
-         maindata:data1
+         Amount:data1
        });
     }
     handleInputChange2(event) {
-       var data2 = this.state.maindata;
+       var data2 = this.state;
        data2.numberofmonths = event.target.value;
-       this.setState({maindata:data2});
+       this.setState({numberofmonths:data2});
     } 
     handleInputChange3(event){
-      var data3 = this.state.maindata;
+      var data3 = this.state;
       data3.whichmonth = event.target.value;
       this.setState({whichmonth:event.target.value});
     } 
     handleSubmit(event){
       event.preventDefault();
-         const consoledata = this.state.maindata
-         const myJSON = JSON.stringify(consoledata);
-         console.log(myJSON);
+      const data1 = event.target.value;
+     // 
+      console.log(data1);
      }
 
- // <label>
-  //Months:
-  //<input>
-    //name="mothts"
-    //type="text"
-    //value={this.state.maindata.months}
-  //  onChange={this.handleInputChange1} />
-//</label>
-//<br />
-//<label>
+      addthedata(event){
+        event.preventDefault();
+          console.log("this is working")
+        }
 
   render() {
     return (
       <div>
-      <form  onSubmit={this.handleSubmit.bind(this)}>
+      <form >
           <label>
           Amount:
-          <input
-            name="Amount"
-            type="number"
-            value={this.state.maindata.Amount}
-            onChange={this.handleInputChange1} />
+          <input name="Amount" type="number"
+           value={this.state.Amount} 
+           onChange={this.handleInputChange1} />
         </label>
+        <button onClick={this.addthedata}>ADD</button>
         <br/>
         <label>
          Number of months:
-          <input 
-          name="numberofmonths"
-          type="number"
-          value={this.state.maindata.numberofmonths}
+          <input name="numberofmonths" type="number"
+         value={this.state.numberofmonths} 
           onChange={this.handleInputChange2}/>
         </label>
+        <button onClick={this.addthedata}>ADD</button>
         <br></br>
+        
          Month
         <label>
-            <input name="whichmonth" 
-            type="text" 
-            value={this.state.maindata.whichmonth} 
+            <input name="whichmonth" type="text" 
+            value={this.state.whichmonth} 
             onChange={this.handleInputChange3} />
         </label>
-         <button>Submit</button>
+
+         <button onClick={this.addthedata}>ADD</button>
+          <div>
+               <button onClick={this.handleSubmit}>CREATE</button>
+          </div>
+        
       </form>
       </div>
     );
