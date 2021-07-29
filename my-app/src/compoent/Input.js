bfab07905b51a0642582ab4585c65ad3bd19f106
Avijit pal <avijitpal20309@gmail.com>
2021-07-30 01:17:19 +0530

    data needs to maped

--- my-app/src/compoent/Input.js
+++ my-app/src/compoent/Input.js
@@ -1,16 +1,25 @@
 //import React, { Component } from 'react'
+import { useState } from "react";
 import React from "react";
 function Form(){
  const [Amount,setAmount] = React.useState(0);
- const [Month,setMonth] = React.useState("");
+ const [Month,setMonth] = React.useState("January");
  const values=[];
- const data=[];
+ //var data=[];
+ const [quants,setquants] = useState({});
   // Form submitting logic, prevent default page refresh 
+
+
   const handleSubmit= event => {
     //here we are pushing the data into the array
     values.push({"Amount":Amount ,"Month":Month});
-    data =[...values];
-    console.log(data)
+    //data = [...values];
+    setquants({
+      ...quants,"Amount":{...(quants[values.Amount]??{}),"Month":event.target.value}
+    })
+
+    console.log(JSON.stringify(quants));
+    //console.log(JSON.stringify(data))
     event.preventDefault();
   }
 
@@ -24,7 +33,7 @@ function Form(){
       <form onSubmit={handleSubmit}>
         <div>
         {/*This is the Amount selection */}
-          <label htmlFor='email'>Amount</label>
+          <label htmlFor='Amount'>Amount</label>
           <input 
             name='Amount'
             type = "number"
@@ -33,9 +42,11 @@ function Form(){
             required 
           />
         </div>
+         {/*This is the for the Selecting the month */}
         <div >
           <label htmlFor='Month'>Month</label>
-           <select name='Month' value={Month} onChange={e=>setMonth(e.target.value) } required >
+           <select name='Month' value={Month} 
+           onChange={e=>setMonth(e.target.value) } required >
            <option name='Month' value="January">January</option>
            <option name='Month' value="February">February</option>
            <option name='Month' value="March">March</option>
@@ -50,12 +61,13 @@ function Form(){
            <option name='Month' value="December">December</option>
            </select>
         </div>
+      {/*Button  to add the value  */}
         <div>
-          <button >ADD</button>
+          <button  >ADD</button>
         </div>
+
       </form>
     )
   }
 export default Form
 //1.placeholder needs to be added
-//
